408fb3ac05c3ed59fa1fcf48005a73e187e695d5
unknown <lkov@post.cz>
2019-11-22 23:21:51 +0100

    edit

--- httptomqtt/index.js
+++ httptomqtt/index.js
@@ -36,10 +36,14 @@ client.on('connect', function () {
     client.subscribe('GPS', function (err) {
         if (!err) {
 
-
+            // client.publish('GPS', JSON.stringify(req.body))
+            //             client.publish('GPS', `
+            // {"app_id":"bustrack","dev_id":"bus_1","hardware_serial":"00D4AD75CAF5BD54","port":2,"counter":174,"payload_raw":"DgEAAAAAAAAAAAAAAA==","payload_fields":{"altitude":123,"latitude":456,"longitude":789,"sats":0,"temperature":999},"metadata":{"time":"2019-11-22T21:04:51.093955727Z","frequency":868.5,"modulation":"LORA","data_rate":"SF8BW125","coding_rate":"4/5","gateways":[{"gtw_id":"eui-b827ebfffec6e5d0","timestamp":2618530724,"time":"2019-11-22T21:04:51.072879Z","channel":2,"rssi":-117,"snr":-5.5,"rf_chain":1,"latitude":49.19936,"longitude":16.57975,"altitude":320}]},"downlink_url":"https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/bustrack/12?key=ttn-account-v2.Qah3ulKsn9CZczqj7ZLzYCruuIn3YyejcFS5gaWkT0c"}
+            // `)
             app.post('/', (req, res) => {
                 // res.json()
                 console.log(new Date(), 'nova poloha');
+                console.log(JSON.stringify(req.body));
                 client.publish('GPS', JSON.stringify(req.body));
 
                 res.send('OK');
